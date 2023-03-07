e4bb70f7327a35a7e3b29db0267656e2bded83c5
HongNguyen302 <123472699+HongNguyen302@users.noreply.github.com>
2023-03-07 10:52:17 -0500

    hands - on project chapter 01
    hands - on project chapter 01

---
+++ project01-04_txt.js
@@ -0,0 +1,12 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Hong Nguyen
+    Date: 03/05/2023  
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
